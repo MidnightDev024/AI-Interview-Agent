d83a05f9dad6155a30779af7df8239652fe1c904
MidnightDev024 <chawlaakash024@gmail.com>
2026-03-07 17:21:47 +0530

    done with routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+import { Route ,Routes } from 'react-router-dom'
 import React from 'react'
 
 function App() {
   return (
-    <div>
-      home page 
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/auth' element={<Auth />} />
+    </Routes>
   )
 }
 
