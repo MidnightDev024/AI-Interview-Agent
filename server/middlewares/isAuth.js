556fd62c68c7a4562fac009087ca96e10a7e1064
MidnightDev024 <chawlaakash024@gmail.com>
2026-03-15 15:20:42 +0530

    refactored : isAuth.js

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -1,22 +1,25 @@
-import jwt from JsonWebToken
-
+import jwt from "jsonwebtoken";
 
 const isAuth = async (req, res, next) => {
     try {
-        let {token} = req.cookies;
+        const { token } = req.cookies;
 
         if(!token) {
-            return res.status(400).json({message: "user does not exist"});
+            return res.status(401).json({message: "unauthorized"});
         }
 
-        const verifyToken = jwt.veriify(token , process.env.JWT_SECRET_KEY);
+        const verifyToken = jwt.verify(token , process.env.JWT_SECRET_KEY);
 
         if(!verifyToken){
-            return res.status(400).json({message:"user does not have valid token "})
+            return res.status(401).json({message:"user does not have valid token "})
         }
+
         req.userId = verifyToken.userId;
+        next();
 
     } catch (error) {
-        return res.status(500).json({message: "Internal server error"});
+        return res.status(401).json({message: "invalid or expired token"});
     }
-}
+};
+
+export default isAuth;
