e24929da79e0046d859e14380414af38a7385a5b
MidnightDev024 <chawlaakash024@gmail.com>
2026-03-15 15:21:03 +0530

    refactored App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
 import { Route ,Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 export const serverURL = "http://localhost:5000"
 
 function App() {
+  useEffect(()=>{
+    const getUser = async () => {
+      try {
+        const result = await axios.get(serverURL + "/api/user/current-user", {withCredentials: true});
+        console.log(result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  getUser();
+  },[])
   return (
     <Routes>
       <Route path='/' element={<Home />} />
