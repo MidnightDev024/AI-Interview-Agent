193c757db27d9a0d4e28350d63f2919269d767b9
MidnightDev024 <chawlaakash024@gmail.com>
2026-03-26 22:58:28 +0530

    refactored openRouter

--- server/services/openRouter.service.js
+++ server/services/openRouter.service.js
@@ -9,7 +9,7 @@ export const askAi = async (message) => {
         }
         const response = await axios.post("https://openrouter.ai/api/v1/chat/completions",
         {
-            model: "OpenAI: GPT-4o-mini",
+            model: "openai/gpt-4o-mini",
             messages: message
         },
         {
