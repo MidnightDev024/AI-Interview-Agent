21707fda09e870faec1358ba740460bdfc50dcd7
MidnightDev024 <chawlaakash024@gmail.com>
2026-03-31 22:30:12 +0530

    interview.route.js

--- server/routes/interview.route.js
+++ server/routes/interview.route.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { upload } from "../middlewares/multer.js";
-import { analyzeResume } from "../controllers/interview.controller.js";
+import { analyzeResume, finishInterview, genrateQuestions, submitAnswer } from "../controllers/interview.controller.js";
 import isAuth from "../middlewares/isAuth.js";
 
 const interviewRouter = express.Router();
 
 interviewRouter.post("/resume",isAuth,upload.single("resume"), analyzeResume);
+
+interviewRouter.post("/generate-questions",isAuth, genrateQuestions);
+
+interviewRouter.post("/submit-answer",isAuth,submitAnswer);
+
+interviewRouter.post("/finish", isAuth, finishInterview);
 
 export default interviewRouter;
