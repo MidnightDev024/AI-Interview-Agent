b2755075eadad1d016bfb07d9877a45d31c336e8
MidnightDev024 <chawlaakash024@gmail.com>
2026-05-08 23:07:46 +0530

    refactored

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://ai-interview-agent-client-bgax.onrender.com',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
