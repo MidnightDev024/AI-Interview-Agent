28779e170f86d7869f03583654f84a1626b975a8
MidnightDev024 <chawlaakash024@gmail.com>
2026-05-09 13:43:24 +0530

    reafctored: multer file

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -12,6 +12,6 @@ const storage = multer.diskStorage({
 
 export const upload = multer({
     storage,
-    limits: { filesize :5 * 1024 * 1024 }, // 5MB limit
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
 
 });
